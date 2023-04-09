9c8420f52106303cf02e3a09d54de54e4f57a649
marienachat <mariem.nachaat@gmail.com>
2023-04-09 04:09:47 -0700

    11

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+/**
+* check_num - check - string there are digit
+* @str: array str
+* return : always 0 (success)
+*/
+
