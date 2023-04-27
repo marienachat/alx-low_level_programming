a8b922fc6bffaa4260e528d7e9df2286521fedd8
marienachat <mariem.nachaat@gmail.com>
2023-04-27 11:45:19 -0700

    1234

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,31 +1,22 @@
+#include <stdio.h>
 #include "lists.h"
-#include <stdio.>
+
 /**
-* _strlen – returns the length of a string
-* @s: the string whose length to check
-* return: integer length of string
-*/
-int _strlen(char *s)
-{
-int i = 0;
-if (!s)
-return (0);
-while (*s++)
-i++;
-return (i);
-}
-/**
-* Print_list – prints alinked lists
-* @h: pointer to first node
+* print_list - prints all the elements of a linked list
+* @h: pointer to the list_t list to print
+* Return: the number of nodes printed
 */
 size_t print_list(const list_t *h)
-size_t  i = 0;
+{
+size_t i = 0;
 while (h)
 {
-printf("[%d] %s\n", _strlen(h->str), h->str ? g->str : "(nil)");
+if (!h->str)
+printf("[0] (nil)\n");
+else
+printf("[%u] %s\n", h->len, h->str);
 h = h->next;
 i++;
 }
 return (i);
 }
-
