2bde95a74bf6aa02b3f254d11b7ee949513be60f
marienachat <mariem.nachaat@gmail.com>
2023-05-05 02:29:27 -0700

    q

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+* get_endianness - function that checks the endianness
+* Return: 0 for big, 1 for little
+*/
+int get_endianness(void)
+{
+unsigned int n = 1;
+char *c = (char *) &n;
+return (*c);
+}
+
